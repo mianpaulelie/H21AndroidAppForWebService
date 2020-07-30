e0df1312fb49280b37e3f0c84adb94fcb48e3ad9
MIAN Paul Elie <s2mianp@inova.local>
2020-07-30 19:07:45 +0000

    android app afichage webservice

---
+++ H21AndroidAppForWebService/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace H21AndroidAppForWebService
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        TextView txtshow;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btnget = FindViewById<Button>(Resource.Id.btnGetWebServiceInfo);
+            txtshow = FindViewById<TextView>(Resource.Id.txtShow);
+            btnget.Click += Btnget_Click;
+
+           
+
+        }
+
+        private void Btnget_Click(object sender, System.EventArgs e)
+        {
+            ContactWebservice.WebServiceH21 webser = new ContactWebservice.WebServiceH21();
+
+            webser.GetJsonAsync();
+            webser.GetJsonCompleted += Webser_GetJsonCompleted;
+        }
+
+        private void Webser_GetJsonCompleted(object sender, ContactWebservice.GetJsonCompletedEventArgs e)
+        {
+            txtshow.Text = e.Result.ToString();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
